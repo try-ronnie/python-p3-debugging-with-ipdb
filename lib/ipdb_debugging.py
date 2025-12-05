a7e61971cd4db0f8ae6769a301765b0eeca5d805
ronniegikandi <kiiruronnie42@gmil.com>
2025-12-05 14:18:33 +0300

    completed lab

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-
-import ipdb
-
 def plus_two(num):
-    answer = num + 2
-    return answer
-
-plus_two(3)
+    num = num + 2   # assign the sum back to num
+    return num
--- lib/ipdb_is_awesome.py
+++ lib/ipdb_is_awesome.py
@@ -6,12 +6,9 @@ def tracing_the_function():
     inside_the_function = "We're inside the function"
     print(inside_the_function)
     print("We're about to stop because of ipdb!")
-    where_the_fuck_the_function = "heyy this is still readable till set trace"
-    print(where_the_fuck_the_function)
     ipdb.set_trace()
     this_variable_hasnt_been_interpreted_yet = \
         "The program froze before it could read me!"
     print(this_variable_hasnt_been_interpreted_yet)
-    
 
 tracing_the_function()
